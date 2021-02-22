711b61a78f85dafee4675ae7e5957ad2da2255c3
mollnn <1084918073@qq.com>
2021-02-22 22:36:18 +0800

    s'type

--- src/main.cpp
+++ src/main.cpp
@@ -11,44 +11,44 @@ extern "C"
 
 int main(int argc, char *argv[])
 {
-	AVFormatContext *pFormatCtx;
+	AVFormatContext *p_format_context;
 	int i, videoindex;
-	AVCodecContext *pCodecCtx;
-	AVCodec *pCodec;
-	AVFrame *pFrame, *pFrameYUV;
+	AVCodecContext *p_codec_context;
+	AVCodec *p_codec;
+	AVFrame *p_frame, *p_frame_yuv;
 	unsigned char *out_buffer;
 	AVPacket *packet;
 	int y_size;
-	int ret, got_picture;
-	struct SwsContext *img_convert_ctx;
+	int flag_ret, flag_got_picture;
+	struct SwsContext *img_convert_context;
 
 	auto filepath = argv[1];
-	//SDL---------------------------
+
 	int screen_w = 0, screen_h = 0;
 	SDL_Window *screen;
-	SDL_Renderer *sdlRenderer;
-	SDL_Texture *sdlTexture;
-	SDL_Rect sdlRect;
+	SDL_Renderer *sdl_renderer;
+	SDL_Texture *sdl_texture;
+	SDL_Rect sdl_rect;
 
 	FILE *fp_yuv;
 
 	av_register_all();
 	avformat_network_init();
-	pFormatCtx = avformat_alloc_context();
+	p_format_context = avformat_alloc_context();
 
-	if (avformat_open_input(&pFormatCtx, filepath, NULL, NULL) != 0)
+	if (avformat_open_input(&p_format_context, filepath, NULL, NULL) != 0)
 	{
 		printf("Couldn't open input stream.\n");
 		return -1;
 	}
-	if (avformat_find_stream_info(pFormatCtx, NULL) < 0)
+	if (avformat_find_stream_info(p_format_context, NULL) < 0)
 	{
 		printf("Couldn't find stream information.\n");
 		return -1;
 	}
 	videoindex = -1;
-	for (i = 0; i < pFormatCtx->nb_streams; i++)
-		if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
+	for (i = 0; i < p_format_context->nb_streams; i++)
+		if (p_format_context->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
 		{
 			videoindex = i;
 			break;
@@ -59,30 +59,30 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	pCodecCtx = pFormatCtx->streams[videoindex]->codec;
-	pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
-	if (pCodec == NULL)
+	p_codec_context = p_format_context->streams[videoindex]->codec;
+	p_codec = avcodec_find_decoder(p_codec_context->codec_id);
+	if (p_codec == NULL)
 	{
 		printf("Codec not found.\n");
 		return -1;
 	}
-	if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0)
+	if (avcodec_open2(p_codec_context, p_codec, NULL) < 0)
 	{
 		printf("Could not open codec.\n");
 		return -1;
 	}
 
-	pFrame = av_frame_alloc();
-	pFrameYUV = av_frame_alloc();
-	out_buffer = (unsigned char *)av_malloc(av_image_get_buffer_size(AV_PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height, 1));
-	av_image_fill_arrays(pFrameYUV->data, pFrameYUV->linesize, out_buffer,
-						 AV_PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height, 1);
+	p_frame = av_frame_alloc();
+	p_frame_yuv = av_frame_alloc();
+	out_buffer = (unsigned char *)av_malloc(av_image_get_buffer_size(AV_PIX_FMT_YUV420P, p_codec_context->width, p_codec_context->height, 1));
+	av_image_fill_arrays(p_frame_yuv->data, p_frame_yuv->linesize, out_buffer,
+						 AV_PIX_FMT_YUV420P, p_codec_context->width, p_codec_context->height, 1);
 
 	packet = (AVPacket *)av_malloc(sizeof(AVPacket));
-	//Output Info-----------------------------
-	av_dump_format(pFormatCtx, 0, filepath, 0);
-	img_convert_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt,
-									 pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_YUV420P, SWS_BICUBIC, NULL, NULL, NULL);
+
+	av_dump_format(p_format_context, 0, filepath, 0);
+	img_convert_context = sws_getContext(p_codec_context->width, p_codec_context->height, p_codec_context->pix_fmt,
+									 p_codec_context->width, p_codec_context->height, AV_PIX_FMT_YUV420P, SWS_BICUBIC, NULL, NULL, NULL);
 
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER))
 	{
@@ -90,8 +90,8 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	screen_w = pCodecCtx->width;
-	screen_h = pCodecCtx->height;
+	screen_w = p_codec_context->width;
+	screen_h = p_codec_context->height;
 
 	screen = SDL_CreateWindow("Player", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 							  screen_w, screen_h,
@@ -103,91 +103,89 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	sdlRenderer = SDL_CreateRenderer(screen, -1, 0);
-	//IYUV: Y + U + V  (3 planes)
-	//YV12: Y + V + U  (3 planes)
-	sdlTexture = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_IYUV, SDL_TEXTUREACCESS_STREAMING, pCodecCtx->width, pCodecCtx->height);
+	sdl_renderer = SDL_CreateRenderer(screen, -1, 0);
+	sdl_texture = SDL_CreateTexture(sdl_renderer, SDL_PIXELFORMAT_IYUV, SDL_TEXTUREACCESS_STREAMING, p_codec_context->width, p_codec_context->height);
 
-	sdlRect.x = 0;
-	sdlRect.y = 0;
-	sdlRect.w = screen_w;
-	sdlRect.h = screen_h;
+	sdl_rect.x = 0;
+	sdl_rect.y = 0;
+	sdl_rect.w = screen_w;
+	sdl_rect.h = screen_h;
 
-	bool running_flag = true;
+	bool flag_running = true;
 
-	while (av_read_frame(pFormatCtx, packet) >= 0 && running_flag)
+	while (av_read_frame(p_format_context, packet) >= 0 && flag_running)
 	{
 		if (packet->stream_index == videoindex)
 		{
-			ret = avcodec_decode_video2(pCodecCtx, pFrame, &got_picture, packet);
-			if (ret < 0)
+			flag_ret = avcodec_decode_video2(p_codec_context, p_frame, &flag_got_picture, packet);
+			if (flag_ret < 0)
 			{
 				printf("Decode Error.\n");
 				return -1;
 			}
-			if (got_picture)
+			if (flag_got_picture)
 			{
-				sws_scale(img_convert_ctx, (const unsigned char *const *)pFrame->data, pFrame->linesize, 0, pCodecCtx->height,
-						  pFrameYUV->data, pFrameYUV->linesize);
+				sws_scale(img_convert_context, (const unsigned char *const *)p_frame->data, p_frame->linesize, 0, p_codec_context->height,
+						  p_frame_yuv->data, p_frame_yuv->linesize);
 
-				SDL_UpdateYUVTexture(sdlTexture, &sdlRect,
-									 pFrameYUV->data[0], pFrameYUV->linesize[0],
-									 pFrameYUV->data[1], pFrameYUV->linesize[1],
-									 pFrameYUV->data[2], pFrameYUV->linesize[2]);
+				SDL_UpdateYUVTexture(sdl_texture, &sdl_rect,
+									 p_frame_yuv->data[0], p_frame_yuv->linesize[0],
+									 p_frame_yuv->data[1], p_frame_yuv->linesize[1],
+									 p_frame_yuv->data[2], p_frame_yuv->linesize[2]);
 
-				SDL_RenderClear(sdlRenderer);
-				SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, &sdlRect);
-				SDL_RenderPresent(sdlRenderer);
+				SDL_RenderClear(sdl_renderer);
+				SDL_RenderCopy(sdl_renderer, sdl_texture, NULL, &sdl_rect);
+				SDL_RenderPresent(sdl_renderer);
 				// SDL_Delay(10);
 			}
 		}
 		av_free_packet(packet);
 
-		SDL_Event event;
-		while (SDL_PollEvent(&event))
+		SDL_Event sdl_event;
+		while (SDL_PollEvent(&sdl_event))
 		{
-			if (event.type == SDL_QUIT)
+			if (sdl_event.type == SDL_QUIT)
 			{
-				running_flag = false;
+				flag_running = false;
 			}
-			if (event.type == SDL_KEYUP)
+			if (sdl_event.type == SDL_KEYUP)
 			{
-				if (event.key.keysym.sym == SDLK_ESCAPE)
+				if (sdl_event.key.keysym.sym == SDLK_ESCAPE)
 				{
-					running_flag = false;
+					flag_running = false;
 				}
 			}
 		}
 	}
 
-	if (running_flag)
+	if (flag_running)
 	{
 		while (1)
 		{
-			ret = avcodec_decode_video2(pCodecCtx, pFrame, &got_picture, packet);
-			if (ret < 0)
+			flag_ret = avcodec_decode_video2(p_codec_context, p_frame, &flag_got_picture, packet);
+			if (flag_ret < 0)
 				break;
-			if (!got_picture)
+			if (!flag_got_picture)
 				break;
-			sws_scale(img_convert_ctx, (const unsigned char *const *)pFrame->data, pFrame->linesize, 0, pCodecCtx->height,
-					  pFrameYUV->data, pFrameYUV->linesize);
+			sws_scale(img_convert_context, (const unsigned char *const *)p_frame->data, p_frame->linesize, 0, p_codec_context->height,
+					  p_frame_yuv->data, p_frame_yuv->linesize);
 			//SDL---------------------------
-			SDL_UpdateTexture(sdlTexture, &sdlRect, pFrameYUV->data[0], pFrameYUV->linesize[0]);
-			SDL_RenderClear(sdlRenderer);
-			SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, &sdlRect);
-			SDL_RenderPresent(sdlRenderer);
+			SDL_UpdateTexture(sdl_texture, &sdl_rect, p_frame_yuv->data[0], p_frame_yuv->linesize[0]);
+			SDL_RenderClear(sdl_renderer);
+			SDL_RenderCopy(sdl_renderer, sdl_texture, NULL, &sdl_rect);
+			SDL_RenderPresent(sdl_renderer);
 			//SDL End-----------------------
 		}
 	}
 
-	sws_freeContext(img_convert_ctx);
+	sws_freeContext(img_convert_context);
 
 	SDL_Quit();
 
-	av_frame_free(&pFrameYUV);
-	av_frame_free(&pFrame);
-	avcodec_close(pCodecCtx);
-	avformat_close_input(&pFormatCtx);
+	av_frame_free(&p_frame_yuv);
+	av_frame_free(&p_frame);
+	avcodec_close(p_codec_context);
+	avformat_close_input(&p_format_context);
 
 	std::cout << "exited." << std::endl;
 
